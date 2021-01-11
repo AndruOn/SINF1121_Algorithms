7c89be4e2f581a628eb68857380fadef26de50c5
Andru Onciul <andru.onciul@student.uclouvain.be>
2021-01-11 16:40:48 +0100

    Mine climb corrigé

--- LSINF1121_PART6_MineClimb/src/main/java/MineClimbing.java
+++ LSINF1121_PART6_MineClimb/src/main/java/MineClimbing.java
@@ -1,9 +1,32 @@
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
+import static org.junit.Assert.assertEquals;
+
 public class MineClimbing {
+    public static class Pos_Dist implements Comparable{
+        int pos;
+        int dist;
+
+        Pos_Dist(int position, int distance) {
+            pos = position;
+            dist = distance;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Pos_Dist o2 = (Pos_Dist) o;
+            return dist - o2.dist;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("pos:%d dist:%d",pos,dist);
+        }
+    }
 
 
     /**
@@ -18,77 +41,100 @@ public class MineClimbing {
      * 0 <= startY, endY < m
      */
     public static int best_distance(int[][] map, int startX, int startY, int endX, int endY) {
-        class distToNode {
-            final int weight;
-            final int v;
 
-            public distToNode(int vertex1, int weight) {
-                this.weight = weight;
-                v = vertex1;
-            }
-        }
-        int nbColumns = map[0].length;
-        int nbLines = map.length;
-        int N = nbColumns * nbLines;
-        boolean[] marked = new boolean[N];
-        int[] distTo = new int[N];
-        for (int i = 0; i < N; i++) {
-            distTo[i] = (int) Double.POSITIVE_INFINITY;
+        int nb_col = map[0].length;
+        int nb_row = map.length;
+        double[] distTo = new double[nb_col * nb_row];
+        for (int i = 0; i < nb_col * nb_row; i++) {
+            distTo[i] = Double.POSITIVE_INFINITY;
         }
 
-        PriorityQueue<distToNode> pq = new PriorityQueue<>(new Comparator<distToNode>() {
-            @Override
-            public int compare(distToNode o1, distToNode o2) {
-                return o1.weight - o2.weight;
-            }
-        });
+        int start = startX * nb_col + startY;
+        int end = endX * nb_col + endY;
+        System.out.printf("start:%d end:%d\n",start,end);
 
-        int start = ind(startX, startY, nbColumns);
+        PriorityQueue<Pos_Dist> pq = new PriorityQueue<>();
         distTo[start] = 0;
-        pq.add(new distToNode(start,0));
+        pq.add(new Pos_Dist(start,0));
 
-        int current;
-        int weight;
-        while (!pq.isEmpty()) {
-            current = pq.poll().v; // a check
-            for (Integer i : adjacents(current, nbColumns, nbLines)) {
-                //System.out.println(String.format("olddist:%d newdist:%d", distTo[i], distTo[current] + height_between(current, i, height)));
-                weight = Math.abs(map[row(current,nbColumns)][column(current,nbColumns)] - map[row(i,nbColumns)][column(i,nbColumns)]);
-                if (distTo[i] > weight + distTo[current] ) {
-                    distTo[i] = weight + distTo[current];
-                    pq.add(new distToNode(i,weight));
+        while(!pq.isEmpty()){
+            Pos_Dist cur = pq.poll();
+
+            //System.out.println("current:"+cur +"  " +Arrays.toString(marked));
+            if (cur.pos == end) {
+                return cur.dist;
+            }
+            int[][] neighbours = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+            for (int[] nei : neighbours) {
+                //System.out.printf("   oldrow:%d old_col:%d",cur.pos / nb_col,cur.pos % nb_col);
+                int row = Math.abs(((cur.pos / nb_col) + nei[0] +nb_row) % nb_row);
+                int col = Math.abs(((cur.pos % nb_col) + nei[1] +nb_col) % nb_col);
+                int adj = row * nb_col + col;
+                //System.out.printf("   row:%d col:%d\n",row,col);
+                int new_dist = cur.dist + Math.abs(map[cur.pos/nb_col][cur.pos%nb_col] - map[row][col]);
+                if (new_dist < distTo[adj]) {
+
+                    Pos_Dist pos_dist= new Pos_Dist(adj,new_dist);
+                    distTo[adj] = new_dist;
+                    //System.out.println("   ->"+pos_dist);
+                    pq.add(pos_dist);
                 }
             }
         }
-
-        int end = ind(endX, endY, nbColumns);
-        //System.out.println(Arrays.toString(distTo));
-        return distTo[end];
+        System.out.println("error pas de chemin trouvé");
+        return -1;
     }
 
-    private static int ind(int X, int Y, int nbColumns) {
-        return X * nbColumns + Y;
+    //JSP pq ca passe les tests, normalement ca devrait utiliser distTo et pas marked dans un dijkstra
+    public static int best_distance_NoWeight(int[][] map, int startX, int startY, int endX, int endY) {
+
+        int nb_col = map[0].length;
+        int nb_row = map.length;
+        boolean[] marked = new boolean[nb_col * nb_row];
+        int start = startX * nb_col + startY;
+        int end = endX * nb_col + endY;
+        System.out.printf("start:%d end:%d\n",start,end);
+
+        PriorityQueue<Pos_Dist> pq = new PriorityQueue<>();
+        pq.add(new Pos_Dist(start,0));
+
+        while(!pq.isEmpty()){
+            Pos_Dist cur = pq.poll();
+            if(marked[cur.pos]) continue;
+            marked[cur.pos] = true;
+
+            //System.out.println("current:"+cur +"  " +Arrays.toString(marked));
+            if (cur.pos == end) {
+                return cur.dist;
+            }
+            int[][] neighbours = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+            for (int[] nei : neighbours) {
+                //System.out.printf("   oldrow:%d old_col:%d",cur.pos / nb_col,cur.pos % nb_col);
+                int row = Math.abs(((cur.pos / nb_col) + nei[0] +nb_row) % nb_row);
+                int col = Math.abs(((cur.pos % nb_col) + nei[1] +nb_col) % nb_col);
+                int adj = row * nb_col + col;
+                //System.out.printf("   row:%d col:%d\n",row,col);
+                if (!marked[adj]) {
+                    int new_dist = cur.dist + Math.abs(map[cur.pos/nb_col][cur.pos%nb_col] - map[row][col]);
+                    Pos_Dist pos_dist= new Pos_Dist(adj,new_dist);
+                    //System.out.println("   ->"+pos_dist);
+                    pq.add(pos_dist);
+                }
+            }
+        }
+        System.out.println("error pas de chemin trouvé");
+        return -1;
     }
 
-    private static int row(int index, int nbColumns) {
-        return index / nbColumns;
-    }
 
-    private static int column(int index, int nbColumns) {
-        return index % nbColumns;
-    }
 
-    private static Iterable<Integer> adjacents(int current, int nbColumns, int nbLines) {
-        int i = row(current, nbColumns);
-        int j = column(current, nbColumns);
-        //System.out.println(String.format("current:%d i:%d j:%d", current, i, j));
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(ind(i, (j + 1) % nbColumns, nbColumns));
-        list.add(ind(i, j - 1 >= 0 ? j - 1 : nbColumns - 1, nbColumns));
-        list.add(ind((i + 1) % nbLines, j, nbColumns));
-        list.add(ind(i - 1 >= 0 ? i - 1 : nbLines - 1, j, nbColumns));
-        //System.out.println(Arrays.toString(list.toArray()));
-        return (Iterable<Integer>) list;
-    }
+    public static void main(String[] args) {
+        int[][] map = new int[][] {
+                {7, 2, 9, 6}, //0,1,2,3
+                {8, 7, 6, 0}, //4,5,6,7
+                {4, 6, 5, 8} //8,9,10,11
+        };
 
+        System.out.println( MineClimbing.best_distance(map, 0, 1, 2, 3));//8
+    }
 }
